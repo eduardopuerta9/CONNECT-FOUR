3d20e926e524e97b84b1a426bd5b86b605e4880b
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-01-26 09:34:54 -0500

    commit message

---
+++ README.md
@@ -0,0 +1,28 @@
+# CONNECT-FOUR
+#### 
+![connect four](https://upload.wikimedia.org/wikipedia/en/7/79/Connect_4_Board_and_Box.jpg) ![outerspace](https://www.spacecentre.nz/resources/learn/universe/img/size_planets_480x270.jpg)
+# BRINGING AN OUT OF THIS WORLD EXPERIENCE TO CONNECT FOUR
+## Date: 1/22/23
+### By:Eduardo Puerta
+#### [GitHub](https://github.com/eduardopuerta9) | [Trello](https://trello.com/b/gYGDhlee/project-1)
+
+### ***Description*** 
+#### An out of this world experience to the renown board game connect four. 
+***
+
+### ***Technologies Used***
+####
+![technologies](https://qph.cf2.quoracdn.net/main-qimg-aea6d70e3db223864d778ee560ec62c0.webp)
+***
+
+### ***Getting Started***
+##### On a board that is 7x6 two players go against each other until someone connects four of their items. This can be done vertically, horizontally, or diagonally. To begin the game click on the row that you want your item into. When there is a winner a message will be displayed, in case of draw just click on the restart button to start all over. 
+
+***
+### ***Screenshots***
+
+***
+### ***Future Updates***
+
+***
+### ***Credits***
---
+++ new.js
@@ -0,0 +1,125 @@
+const board = document.querySelector('#board')
+const modalContainer = document.querySelector('#modal-container')
+const modalMessage = document.querySelector('#modal-message')
+const resetButton = document.querySelector('#reset')
+resetButton.onclick = () => {
+  location.reload()
+}
+const redTurn = 1
+const yellowTurn = 2
+const pieces = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]
+let playerTurn = redTurn
+let hoverColumn = -1
+for (let i = 0; i < 42; i++) {
+  let cell = document.createElement('div')
+  cell.className = 'cell'
+  board.appendChild(cell)
+
+  cell.onmouseenter = () => {
+    onMouseEnteredColumn(i % 7)
+  }
+  cell.onclick = () => {
+    onColumnClicked(i % 7)
+  }
+}
+
+function onColumnClicked(column) {
+  let availableRow = pieces
+    .filter((_, index) => index % 7 === column)
+    .lastIndexOf(0)
+  if (availableRow === -1) {
+    return
+  }
+  pieces[availableRow * 7 + column] = playerTurn
+  let cell = board.children[availableRow * 7 + column]
+  let piece = document.createElement('div')
+  piece.className = 'piece'
+  piece.dataset.placed = true
+  piece.dataset.player = playerTurn
+  cell.appendChild(piece)
+
+  function checkGameWinOrDraw() {
+    if (!pieces.includes(0)) {
+      modalContainer.style.display = 'block'
+      modalMessage.textContent = 'Draw'
+    }
+    if (hasPlayerWon(playerTurn, pieces)) {
+      modalContainer.style.display = 'block'
+      modalMessage.textContent = `${
+        playerTurn === redTurn ? 'Red' : 'Yellow'
+      } WON!`
+      modalMessage.dataset.winner = playerTurn
+    }
+  }
+  if (playerTurn === redTurn) {
+    playerTurn = yellowTurn
+  } else {
+    playerTurn = redTurn
+  }
+  updateHover()
+}
+function updateHover() {
+  let unPlacedPiece = document.querySelector("[data-placed='false']")
+  if (unPlacedPiece) {
+    unPlacedPiece.parentElement.removeChild(unPlacedPiece)
+  }
+  if (pieces[hoverColumn] === 0) {
+    let cell = board.children[hoverColumn]
+    let piece = document.createElement('div')
+    piece.className = 'piece'
+    piece.dataset.placed = false
+    piece.dataset.player = playerTurn
+    cell.appendChild(piece)
+  }
+}
+
+function onMouseEnteredColumn(column) {
+  hoverColumn = column
+  updateHover()
+}
+function hasPlayerWon(playerTurn, pieces) {
+  for (let index = 0; index < 42; index++) {
+    if (
+      index % 7 < 4 &&
+      pieces[index] === playerTurn &&
+      pieces[index + 1] === playerTurn &&
+      pieces[index + 2] === playerTurn &&
+      pieces[index + 3] === playerTurn
+    ) {
+      return true
+    }
+    if (
+      index < 21 &&
+      pieces[index] === playerTurn &&
+      pieces[index + 7] === playerTurn &&
+      pieces[index + 14] === playerTurn &&
+      pieces[index + 21] === playerTurn
+    ) {
+      return true
+    }
+    if (
+      index % 7 < 4 &&
+      index < 18 &&
+      pieces[index] === playerTurn &&
+      pieces[index + 8] === playerTurn &&
+      pieces[index + 16] === playerTurn &&
+      pieces[index + 24] === playerTurn
+    ) {
+      return true
+    }
+    if (
+      index % 7 >= 3 &&
+      index < 21 &&
+      pieces[index] === playerTurn &&
+      pieces[index + 6] === playerTurn &&
+      pieces[index + 12] === playerTurn &&
+      pieces[index + 18] === playerTurn
+    ) {
+      return true
+    }
+  }
+  return false
+}
