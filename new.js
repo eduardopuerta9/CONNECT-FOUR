f5df6a2582bd7f6632418a33ca3cd033ad301b92
Eduardo Puerta <puerta.eduardo10@gmail.com>
2023-01-26 10:14:47 -0500

    Created reset button, player turn, and pieces variables

--- new.js
+++ new.js
@@ -1,3 +1,13 @@
 const board = document.querySelector('#board')
 const modalContainer = document.querySelector('#modal-container')
 const modalMessage = document.querySelector('#modal-message')
+const resetButton = document.querySelector('#reset')
+resetButton.onclick = () => {
+  location.reload()
+}
+const redTurn = 1
+const yellowTurn = 2
+const pieces = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]
